08ade86152a5a56ee4abea2c35d14b43898f0e98
sajanakarunarathna <sajanaashen@gmail.com>
2024-08-05 13:30:32 +0530

    404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,25 +1,39 @@
-import Link from 'next/link'
 import Image from "next/image";
+import Link from 'next/link';
 
-export default function NotFound() {
+export default function ContactUs() {
   return (
-    <div className="text-center font-poppins">
-      <Image
-        src="/404.svg"
-        width={100}
-        height={100}
-        alt="404"
-        className="w-2/5 mx-auto"
-      />
-      <Link
-        href="/"
-        className="button"
-        style={{ position: "relative", top: "-100px" }}
-      >
-        <button className="bg-[#31AFA9] hover:bg-green-900 text-white  py-2 px-7 rounded-full">
-          Go back to Home
-        </button>
-      </Link>
-    </div>
+    <>
+      <div>
+        <section className="bg-white dark:bg-gray-900 font-poppins">
+          <div className="mx-auto max-w-screen-xl">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-16 my-8 md:my-16">
+              <div className="md:mt-16 px-5 text-center md:text-left">
+                <h1 className="mb-4 text-3xl md:text-5xl font-semibold">
+                  Oops!
+                </h1>
+                <p className="text-gray-800 text-3xl font-medium mt-7">
+                  We can't seem to find the page you're looking for
+                </p>
+                <Link href="/">
+                  <button className="bg-[#31afa9] hover:bg-green-900 text-xl text-white py-2 px-7 mt-16 rounded-full">
+                    Reload Page
+                  </button>
+                </Link>
+              </div>
+              <div>
+                <Image
+                  src="/404.svg"
+                  width={100}
+                  height={100}
+                  alt="404"
+                  className="w-full mx-auto"
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -103,7 +103,7 @@ export default function Home() {
             sectionsRef.current[0] = el;
           }}
           data-index={0}
-          className="opacity-0 transition-opacity duration-[4000ms] ease-in delay-700 bg-[#F7FFFF] dark:bg-gray-900 p-20"
+          className="opacity-0 transition-opacity ease-in delay-700 bg-[#F7FFFF] dark:bg-gray-900 p-20"
         >
           <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
             <p className="mb-12 font-medium text-2xl text-[#3A3A3A] font-poppins">
@@ -130,7 +130,7 @@ export default function Home() {
             sectionsRef.current[1] = el;
           }}
           data-index={1}
-          className="opacity-0 transition-opacity duration-[4000ms] ease-in delay-700 bg-white dark:bg-gray-900"
+          className="opacity-0 transition-opacity ease-in delay-700 bg-white dark:bg-gray-900"
         >
           <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
             <div className="mx-auto max-w-screen-xl text-left mb-8 lg:mb-16">
@@ -153,7 +153,7 @@ export default function Home() {
             sectionsRef.current[2] = el;
           }}
           data-index={2}
-          className="opacity-0 transition-opacity duration-[4000ms] ease-in delay-700 bg-[#F7FFFF] dark:bg-gray-900 p-16"
+          className="opacity-0 transition-opacity ease-in delay-700 bg-[#F7FFFF] dark:bg-gray-900 p-16"
         >
           <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
             <h2 className="max-w-2xl lg:mb-5 mb-4 text-4xl font-normal tracking-tight leading-none md:text-5xl xl:text-4xl text-gray-900 dark:text-white">
@@ -308,7 +308,7 @@ export default function Home() {
             sectionsRef.current[3] = el;
           }}
           data-index={3}
-          className="opacity-0 transition-opacity duration-[4000ms] ease-in delay-700 bg-white dark:bg-gray-900"
+          className="opacity-0 transition-opacity ease-in delay-700 bg-white dark:bg-gray-900"
         >
           <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
             <div>
@@ -379,7 +379,7 @@ export default function Home() {
             sectionsRef.current[4] = el;
           }}
           data-index={2}
-          className="opacity-0 transition-opacity duration-[4000ms] ease-in delay-700 bg-[#F7FFFF] dark:bg-gray-900 p-16 font-poppins"
+          className="opacity-0 transition-opacity ease-in delay-700 bg-[#F7FFFF] dark:bg-gray-900 p-16 font-poppins"
         >
           <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 text-center">
             <div className="text-4xl font-medium mb-8">Get to know us</div>
